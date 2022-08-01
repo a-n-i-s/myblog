83e3201a686fd872f4589a982aaab740213b1580
a-n-i-s <anisur.ra7man@gmail.com>
2022-08-01 11:32:27 +0600

    post basic model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
-
+from .models import Tag,Catagory,Post
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+            ('title'),
+            ('author','catagory'),
+            ('created_at','updated_at','hits'),          
+            ),
+            
+        }),
+        (None, {
+            "fields": (
+            'body',          
+            ),
+        }),
+        
+        (None, {
+            "fields": (
+            'tags',          
+            ),
+        }),
+        
+    )
+    
+    
+    readonly_fields=['created_at','updated_at','hits']
+
+@admin.register(Catagory)
+class CatagoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+from user.models import Author
+from django.urls import reverse
 # Create your models here.
+
+class Post(models.Model):
+  PRIVACY=[
+    (0,'public'),
+    (1,'readers only'),
+  ]
+  
+  title=models.CharField(max_length=200)
+  body=models.TextField()
+  created_at=models.DateTimeField(auto_now_add=True)
+  updated_at=models.DateTimeField(auto_now=True)
+  author=models.ForeignKey(Author,on_delete=models.CASCADE)
+  hits=models.IntegerField(default=0,editable=False)
+  privacy=models.IntegerField(choices=PRIVACY,default=0)
+  tags=models.ManyToManyField('Tag',blank=True)
+  catagory=models.ForeignKey('Catagory',on_delete=models.CASCADE)
+  def __str__(self):
+      return self.title
+
+  def get_absolute_url(self):
+      return reverse("post_detail", kwargs={"pk": self.pk})
+
+
+
+class Tag(models.Model):
+
+    name=models.CharField(max_length=100)
+
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Tag_detail", kwargs={"pk": self.pk})
+
+class Catagory(models.Model):
+    parent=models.ForeignKey('self',null=True,blank=True,default=None,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Catagory_detail", kwargs={"pk": self.pk})
