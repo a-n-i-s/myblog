69594fc2bb4c6dc6e73bbfc50e659ea464677e61
a-n-i-s <anisur.ra7man@gmail.com>
2022-08-01 09:43:31 +0600

    basic author model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
+from atexit import register
 from django.contrib import admin
-
+from .models import Author
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
--- user/models.py
+++ user/models.py
@@ -1,7 +1,20 @@
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
-
+from django.db import models
+from django.contrib.auth import get_user_model
 class User(AbstractUser):
   pass
 
 
+class Author(models.Model):
+  STATUS=[
+    
+    (0,'normal'),
+    (1,'blocked'),
+  ]
+  user=models.OneToOneField(get_user_model(),on_delete=models.CASCADE)
+  name=models.CharField(max_length=200)
+  about=models.TextField()
+  status=models.IntegerField(choices=STATUS,default=0)
+
+  def __str__(self):
+    return str(self.user)
