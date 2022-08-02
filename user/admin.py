56a51815c4b263ce3a230459f6c164fcad3d19bc
a-n-i-s <anisur.ra7man@gmail.com>
2022-08-02 14:14:46 +0600

    added user admin

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,13 @@
 from atexit import register
 from django.contrib import admin
-from .models import Author
+from .models import Author,User
 # Register your models here.
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(User)    
+class UserAdmin(admin.ModelAdmin):
     pass
 
